ec399214a69b6ba5f4520d1ff1162588d8fa28e8
alison <alisonadriel@hotmail.com>
2021-04-05 00:35:12 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// Connect DB
+
+require('./config/db.config');
+
+const app = express();
+
+// Middlewares
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Rotas
+
+//const projectRoutes = require('./routes/project.routes');
+//const authRoutes = require('./routes/auth.routes');
+
+// Rotas Publicas
+//app.use('/auth', authRoutes);
+
+// Middleware de autenticação
+
+const authMiddleware = require('./middlewares/auth.middleware');
+
+app.use(authMiddleware);
+
+
+// Rotas Privadas que precisam de jwt
+
+
+
+// exportar o meu app
+
+module.exports = app;
