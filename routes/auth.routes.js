cdcd66aa2662b659d5b0eac344c8bc6ad120dedb
alison <alisonadriel@hotmail.com>
2021-04-05 21:10:40 -0300

    rota authenticao sponsor

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,13 +1,13 @@
 const bcrypt = require('bcryptjs');
 const administratorDao = require('../repository/administrator.dao')
+const sponsorDao = require('../repository/sponsor.dao')
 const jwt = require('jsonwebtoken');
 const { Router } = require('express');
 
-const routerAdm = Router();
+const routerAuth = Router();
 
-routerAdm.post('/signup', async(req, res) =>{
+routerAuth.post('/signupAdm', async(req, res) =>{
     try {
-
         const { name, phone, email, age, imageUrl, password } = req.body
 
         if(!name && !phone && !email && !age && !imageUrl && !password){
@@ -20,9 +20,8 @@ routerAdm.post('/signup', async(req, res) =>{
     }
 })
 
-routerAdm.post('/login', async(req, res) =>{
+routerAuth.post('/loginAdm', async(req, res) =>{
     try {
-        
         const { name, phone, email, age, imageUrl, password } = req.body
         const administrador = await administratorDao.findAdministrator(email)
         
@@ -49,5 +48,48 @@ routerAdm.post('/login', async(req, res) =>{
         
         res.status(500).json();
     }
+})    
+routerAuth.post('/signup', async(req, res) =>{
+    try {
+        const { name, phone, email, age, imageUrl, password } = req.body
+
+        if(!name && !phone && !email && !age && !imageUrl && !password){
+            return res.status(400).json({message: 'Todos os campos são obrigatórios'})
+        }
+        const sponsor = await sponsorDao.register(req.body)
+        res.status(201).json(sponsor)
+        console.log(`console sponsor ${sponsor}`)
+    } catch (error) {
+        res.status(500).json({message:'Erro ao registrar novo Patrocinador'})
+    }
 })
-module.exports = routerAdm;
+routerAuth.post('/login', async(req, res) =>{
+    try {
+        const { name, phone, email, age, imageUrl, password} = req.body
+        const sponsor = await sponsorDao.findSponsor(email)
+        
+        if(!sponsor){
+            return res.status(400).json({message:'Login não autorizado, entre com senha e email novamente'})
+        } 
+        const compareHash = bcrypt.compareSync(password, sponsor.passwordHash)
+      
+        if(!compareHash){
+             return res.status(400).json({message:'Login não autorizado, entre com senha e email novamente'})
+        }
+        const payload = {
+            email: sponsor.email,
+            id: sponsor._id
+        };
+        
+        const token = jwt.sign(
+            payload,
+            process.env.SECRET_JWT,
+            {expiresIn: '1day'}
+        )
+         res.status(200).json({ payload,token})
+    } catch (error) {
+        
+        res.status(500).json();
+    }
+})
+module.exports = routerAuth;
