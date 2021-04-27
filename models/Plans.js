8633d4f517bf4e54422f0a7429ddbb8f77a3feab
alison <alisonadriel@hotmail.com>
2021-04-27 19:09:24 -0300

    ajuste pull origin main

---
+++ models/Plans.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+
+
+const plansSchema = new Schema(
+    {
+        name: {
+            type: String,
+            time: true,
+            unique: true,
+            required: [true, 'Nome é obrigatório.'],
+        },
+        price: {
+            type: String,
+            required: [true, 'Valor do plano é obrigatório'],
+        },
+        foto: {
+            type: Number,
+            required: [ true, 'Quantidade de fotos é obrigatório.']
+        },
+        dayUse: {
+            type: Number,
+             required: [ true, 'Quantidade de day use é obrigatório.']
+        },
+        video: {
+            type: Number,
+             required: [ true, 'Quantidade de videos é obrigatório.']
+        },
+    },
+    {
+        timestamps: true
+    }
+);
+module.exports = model('Plans', plansSchema);
